72ffdc97624df0d17e3f23744fcdc690377f309b
吴临波 <wulinbo@zibolan.com>
2017-02-17 17:03:50 +0800

    learn

--- js/cart.js
+++ js/cart.js
@@ -5,14 +5,18 @@ var vm  = new Vue({
     el:'#app',
     data:{
         productList:[],
-        totalMoney: 0
+        totalMoney: 0,
+        checkAllFlag:false
     },
     filters:{
-
+        formatMoney:function (val) {
+            return "￥"+val.toFixed(2);
+        }
     },
     mounted:function(){
-
-        this.cartView();
+        this.$nextTick(function () {
+            vm.cartView();
+        })
     },
     methods:{
         cartView:function () {
@@ -21,6 +25,39 @@ var vm  = new Vue({
                 _this.productList = res.body.result.productList;
                 _this.totalMoney = res.body.result.totalMoney;
             });
+        },
+        changeMoney:function (productList,way) {
+            if(way>0){
+               productList.productQuentity++;
+            }else {
+                productList.productQuentity--;
+                if(productList.productQuentity<1){
+                    productList.productQuentity = 1;
+                }
+            }
+
+        },
+        changeCheck:function (item) {
+            if(typeof item.checked == 'undefined'){
+                //Vue.set(item,"checked",true);
+                this.$set(item,'checked',true)
+            }else {
+                item.checked = !item.checked;
+            }
+        },
+        checkAll:function (flag) {
+            this.checkAllFlag = flag;
+            var _this = this;
+            this.productList.forEach(function (item, index) {
+                if(typeof item.checked == 'undefined'){
+                    _this.$set(item,'checked',_this.checkAllFlag)
+                }else {
+                    item.checked = _this.checkAllFlag;
+                }
+            })
         }
     }
+});
+Vue.filter("money",function (val,type) {
+    return "￥"+val.toFixed(2)+type;
 })
